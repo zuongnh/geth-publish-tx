19a03c3b16fadf93e7b17f94a02daecb9cf9b7a6
Nguyen Duong <muoi07052001@gmail.com>
2023-01-05 08:44:09 +0700

    refactor

--- contract_write.go
+++ contract_write.go
@@ -16,12 +16,14 @@ import (
 )
 
 func main() {
+	// -------------- SET UP --------------
 	client, err := ethclient.Dial("wss://bsc-testnet.nodereal.io/ws/v1/e9a36765eb8a40b9bd12e680a1fd2bc5")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	privateKey, err := crypto.HexToECDSA("341ff1cc75cc7a421982cc904c075c4819b528d80182f36cfab3530c953f3fb6")
+	// privateKey, err := crypto.HexToECDSA("341ff1cc75cc7a421982cc904c075c4819b528d80182f36cfab3530c953f3fb6")
+	privateKey, err := crypto.HexToECDSA("57d7a2a1188476d970f71e7dc4084281a66499345e5a1421e0dc15b1e74c631a")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,31 +47,50 @@ func main() {
 
 	auth := bind.NewKeyedTransactor(privateKey)
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)     // in wei
-	auth.GasLimit = uint64(300000) // in units
+	auth.Value = big.NewInt(0)      // in wei
+	auth.GasLimit = uint64(1000000) // in units
 	auth.GasPrice = gasPrice
 
+	// -------------- CREATE SC INSTANCE --------------
 	address := common.HexToAddress("0xa66feE583714f35cbB20b8322F50cF5AD21F5388")
 	instance, err := store.NewStore(address, client)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// -------------- PUBLISH TX --------------
+	publish_open_pack_tx(instance, auth)
+
 	zombieId := big.NewInt(104)
 	targetId := big.NewInt(4)
+	publish_attack_tx(instance, auth, zombieId, targetId)
 
-	// Publish Attack transaction
+	get_zombies_of(instance, fromAddress)
+}
+
+// Publish openStarterPack transaction
+func publish_open_pack_tx(instance *store.Store, auth *bind.TransactOpts) {
+	tx, err := instance.OpenStarterPack(auth)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("tx sent: %s", tx.Hash().Hex())
+}
+
+// Publish Attack transaction
+func publish_attack_tx(instance *store.Store, auth *bind.TransactOpts, zombieId *big.Int, targetId *big.Int) {
 	tx, err := instance.Attack(auth, zombieId, targetId)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Printf("tx sent: %s", tx.Hash().Hex())
+}
 
-	// result, err := instance.Zombies(nil, zombieId)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println(result) // "bar"
+// Publish get all Zombies of an address transaction
+func get_zombies_of(instance *store.Store, owner common.Address) {
+	res, err := instance.GetZombieOf(nil, owner)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Result: %v", res)
 }
